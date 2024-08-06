47449673dc0e10db02884c9464d4e9f805179371
William Ureta <will@offshorly.com>
2024-08-06 14:18:49 +0800

    clean the code

--- scripts/checkCodeSnippetExist.ts
+++ scripts/checkCodeSnippetExist.ts
@@ -3,26 +3,25 @@ import * as path from 'path';
 
 function checkCodeSnippets(filePath: string): void {
   try {
-    // Read the contents of the file
+    // read the contents of the file
     const fileContent: string = fs.readFileSync(filePath, 'utf-8');
 
-    // Regular expression to match triple backticks
+    // regular expression to match triple backticks
     const codeBlockRegex: RegExp = /```[\s\S]*?```/;
 
-    // Check if the file contains code snippets
+    // check if the file contains code snippets
     if (!codeBlockRegex.test(fileContent)) {
       throw new Error('No code snippets found in the Markdown file.');
     }
 
-    console.log('Code snippets found in the Markdown file.');
-    // Proceed with further actions if needed
+    // console.log('Code snippets found in the Markdown file.');
+    // proceed with further actions if needed
   } catch (error) {
     if (error instanceof Error) {
       console.error(`Error: ${error.message}`);
     } else {
       console.error('An unknown error occurred.');
     }
-    process.exit(1);
   }
 }
 
